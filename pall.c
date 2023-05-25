97dc1e99a0ca555a23a1681fdffe3b88ba9bb4a4
kucheli mutambi <kuchelideogratius@gmail.com>
2023-05-25 15:02:36 +0300

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,21 +1,41 @@
 #include "monty.h"
 
-/**
- * pall - print out all the stack
- * @stack: the stack
- * @line_num: line number
- */
+void push(stack_t **stack, unsigned int line_num)
+{
+    /* Parse the argument */
+    int value = 0;
+    /* ... Code to parse the argument using atoi or similar ... */
+
+    /* Handle invalid argument */
+    if (!valid_argument)
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line_num);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Create a new node and set its value */
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->n = value;
+
+    /* Insert the new node at the top of the stack */
+    new_node->prev = NULL;
+    new_node->next = *stack;
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+    *stack = new_node;
+}
+
 void pall(stack_t **stack, unsigned int line_num)
 {
-	stack_t *current;
-
-	(void)line_num;
-	if (*stack == NULL)
-		return;
-	current = *stack;
-	while (current != NULL)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
+    stack_t *current = *stack;
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
 }
